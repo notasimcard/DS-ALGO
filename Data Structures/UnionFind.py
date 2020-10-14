54485fd607026489be224aea77d204e1229f6bf1
Asim Thapa <asimtut@gmail.com>
2020-10-14 17:02:20 -0400

    Started Union Find

--- Data Structures/UnionFind.py
+++ Data Structures/UnionFind.py
@@ -1,5 +1,6 @@
 """
-Union Find
+Union Find / Disjoint Set
 - To begin using Union Find, first construct bijection (For array based union find)
-    - Bijection: Mapping between your object and the integers in the range [0, n)
+    - Bijection: Mapping between your object and the integers in the range [0, n) where n = length of your input
+- At first, every value is a root node
 """
