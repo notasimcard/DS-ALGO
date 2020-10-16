85eb71a4805eda84c90f584934788a9e9195b1d8
notasimcard <asimtut@gmail.com>
2020-10-16 10:46:15 -0400

    Added Binary Search Tree

---
+++ Data Structures/BinarySearchTrees.py
@@ -0,0 +1,12 @@
+"""
+Uses:
+    1. Map and Abstract Data Type(ADT)s implementation
+    2. Red Black Trees
+    3. AVL Trees
+    4. Splay Trees
+    5. Syntax Trees
+    6. Treap
+    7. Binary heaps, etc
+"""
+
+#TODO: Check if binary search tree is valid or not
