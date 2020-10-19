9de9cff996481c0f998b6661c337370bb4bce3e6
notasimcard <asimtut@gmail.com>
2020-10-19 19:46:46 -0400

    BST -Finished node removal, optimization needed

--- Data Structures/BinarySearchTrees.py
+++ Data Structures/BinarySearchTrees.py
@@ -8,13 +8,7 @@ Uses:
     6. Treap
     7. Binary heaps, etc
 """
-#TODO: Binary Removal
-    # Case 1: Node to remove is a leaf node
-    # Case 2: Node to remove has only left subtree
-    # Case 3: Node to remove has only right subtree
-    # Case 4: Node to remove has both subtrees
-        # I) Successor can be the largest value in the left subtree
-        # II) or, Successor can be the smallest value in the right subtree
+#TODO: optimize node removal
 
 class Node:
     # Tree Node
@@ -64,6 +58,67 @@ class BST:
                 curr_node.count += 1
                 return
 
+
+    def remove(self, key):
+        curr_node = self.root
+        parent_node = None
+        # Case I: BST is empty
+        if self.root is None:
+            return -1
+        # Case II: key is not present in BST
+        while curr_node is not None:
+            if key < curr_node.value:
+                parent_node = curr_node
+                curr_node = curr_node.left
+            elif key > curr_node.value:
+                parent_node = curr_node
+                curr_node = curr_node.right
+            # Found the node with key
+            else:
+                # Case III: In this implementation, there is
+                # count instance variable which holds duplicate
+                # counts. if cout > 1, decrement count
+                if curr_node.count > 1:
+                    curr_node.count -= 1
+                    return 0
+                # Case IV: delete node is leaf
+                elif curr_node.left is None and curr_node.right is None:
+                    curr_node = None
+                # Case V: delete node only has left subtree child
+                elif curr_node.right is None:
+                    temp = curr_node.left
+                    if parent_node is None:
+                        self.root = temp
+                    elif parent_node.left is not None and parent_node.left.value == key:
+                        parent_node.left = temp
+                    else:
+                        parent_node.right = temp
+                    curr_node = None
+                # Case VI: delete node only has right subtree child
+                elif curr_node.left is None:
+                    temp = curr_node.right
+                    if parent_node is None:
+                        self.root = temp
+                    elif parent_node.left is not None and parent_node.left.value == key:
+                        parent_node.left = temp
+                    else:
+                        parent_node.right = temp
+                    curr_node = None
+                # Case VII: delete node has both children
+                else:
+                    # Find the smallest value from the right subtree
+                    temp = curr_node.right
+                    while temp.left:
+                        parent_node = temp
+                        temp = temp.left
+                    curr_node.value = temp.value
+                    curr_node.count = temp.count
+                    # Return of case VI + IV
+                    parent_node.left = temp.right
+                    temp = None
+            return 0
+        return -1
+    
 
     def traversals(self, traverse="in_order"):
         if traverse == "in_order":
@@ -152,3 +207,7 @@ bst.in_order(bst.root)
 print("\n--------------------")
 bst.remove(10)
 bst.in_order(bst.root)
+print("\n--------------------")
+bst.remove(10)
+bst.in_order(bst.root)
+
